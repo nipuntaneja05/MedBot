6108d434d78703eb7225f8b2863e8e3cd24d701b
pknowledge <xyz@gmail.com>
2025-02-10 17:06:06 +0530

    1st commit

---
+++ .history/setup_20250210170128.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Gen Ai Project',
+    version='0.0',
+    author='Nipun taneja',
+    author_email='entbappy73@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
